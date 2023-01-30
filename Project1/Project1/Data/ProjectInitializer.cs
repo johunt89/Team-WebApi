37e5c7aab73a1834b650d5c09a9aaf55a3b8940a
johunt89 <79724849+johunt89@users.noreply.github.com>
2023-01-30 17:20:01 -0500

    seed data created

--- Project1/Project1/Data/ProjectInitializer.cs
+++ Project1/Project1/Data/ProjectInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Project1.Models;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Project1.Data
 {
@@ -13,6 +14,8 @@ namespace Project1.Data
 
             try
             {
+
+                Random random = new Random();
                 //League Data
                 if (!context.Leagues.Any())
                 {
@@ -20,12 +23,63 @@ namespace Project1.Data
                         new League
                         {
                             Name = "Premier League"
-                        });
+                        },
+                        new League 
+                        { 
+                            Name = "Major League"
+                        },
+                        new League
+                        {
+                            Name = "Pro League"
+                        },
+                        new League
+                        {
+                            Name = "Little League"
+                        }
+                        );
                     context.SaveChanges();
                 }
                 //Team Data
-
+                if (!context.Teams.Any())
+                {
+                    context.Teams.AddRange(
+                    new Team
+                    {
+                        Name = "Toronto Laughs"
+                    },
+                    new Team
+                    {
+                        Name = "Hamilton Steel"
+                    },
+                    new Team
+                    {
+                        Name = "Sudbury Nickles"
+                    },
+                    new Team
+                    {
+                        Name = "Winnipeg Moose"
+                    },
+                    new Team
+                    {
+                        Name = "Saskatoon Flatlands"
+                    }
+                    );
+                }
                 //Player Data
+                if (!context.Players.Any())
+                {
+                    DateTime startDOB = DateTime.Today;
+                    context.Players.AddRange(
+                        new Player
+                        {
+                            FirstName = "Jim",
+                            LastName = "Othy",
+                            Jersey = "12",
+                            DOB = startDOB.AddDays(-random.Next(6500, 25000)),
+                            FeePaid = 100.00,
+                            EMail = "jimothy@jimjim.com"
+                        });
+                }
             }
             catch (Exception ex)
             {
