4b45dbeadf51ce8c6f91a835a8b66a686d5d3300
johunt89 <79724849+johunt89@users.noreply.github.com>
2023-02-01 20:44:54 -0500

    scaffolded controllers

---
+++ Project1/Project1/Controllers/LeaguesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project1.Data;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaguesController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public LeaguesController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Leagues
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<League>>> GetLeagues()
+        {
+            return await _context.Leagues.ToListAsync();
+        }
+
+        // GET: api/Leagues/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<League>> GetLeague(string id)
+        {
+            var league = await _context.Leagues.FindAsync(id);
+
+            if (league == null)
+            {
+                return NotFound();
+            }
+
+            return league;
+        }
+
+        // PUT: api/Leagues/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLeague(string id, League league)
+        {
+            if (id != league.Code)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(league).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LeagueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Leagues
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<League>> PostLeague(League league)
+        {
+            _context.Leagues.Add(league);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LeagueExists(league.Code))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetLeague", new { id = league.Code }, league);
+        }
+
+        // DELETE: api/Leagues/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLeague(string id)
+        {
+            var league = await _context.Leagues.FindAsync(id);
+            if (league == null)
+            {
+                return NotFound();
+            }
+
+            _context.Leagues.Remove(league);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LeagueExists(string id)
+        {
+            return _context.Leagues.Any(e => e.Code == id);
+        }
+    }
+}
